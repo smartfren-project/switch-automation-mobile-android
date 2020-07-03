56d0be38390bd47a742c1a2938c09992ec6eec7f
Faizal Prakoso <faizalprakoso@Faizals-MacBook-Pro.local>
2020-07-03 11:11:54 +0700

    change web element to string

--- src/main/java/constants/ObjectElement.java
+++ src/main/java/constants/ObjectElement.java
@@ -1,10 +1,10 @@
-package Data;
+package constants;
 
-import Operation.Driver;
+import operation.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class ObjectElement extends Driver {
+public class ObjectElement extends BaseTest {
 
     public interface LandingPageObject {
         String btnLogin = "com.smartfren.switchmobile:id/skip";
@@ -17,28 +17,38 @@ public class ObjectElement extends Driver {
     public interface LoginPageObject {
         String btnBack = "com.smartfren.switchmobile:id/imageView2";
         String btnGoLogin = "com.smartfren.switchmobile:id/signup";
-        WebElement btnSignUp = driver.findElement(By.id("com.smartfren.switchmobile:id/login"));
+        String btnSignUp = "com.smartfren.switchmobile:id/login";
         String txtWelcomeTitle = "com.smartfren.switchmobile:id/textView1";
         String txtWelcomeDesc = "com.smartfren.switchmobile:id/firstName";
         String inputUserName = "com.smartfren.switchmobile:id/email";
         String btnSubmitLogin = "com.smartfren.switchmobile:id/registration_button";
         String txtOtherWaysTitle = "com.smartfren.switchmobile:id/textView13";
-        WebElement btnOtherMethods = driver.findElement(By.id("com.smartfren.switchmobile:id/social_media_btn"));
-        WebElement txtQuestAccountTitle = driver.findElement(By.id("com.smartfren.switchmobile:id/already_have_account"));
-        WebElement btnQuestSignUp = driver.findElement(By.id("com.smartfren.switchmobile:id/login_here"));
-        WebElement txtVariousLoginTitle = driver.findElement(By.id("com.smartfren.switchmobile:id/txt_get_various_rewards"));
-        WebElement btnGoogle = driver.findElement(By.id("com.smartfren.switchmobile:id/btn_login_google"));
-        WebElement btnFacebook = driver.findElement(By.id("com.smartfren.switchmobile:id/btn_login_facebook"));
+        String btnOtherMethods = "com.smartfren.switchmobile:id/social_media_btn";
+        String txtQuestAccountTitle = "com.smartfren.switchmobile:id/already_have_account";
+        String btnQuestSignUp = "com.smartfren.switchmobile:id/login_here";
+        String txtVariousLoginDesc = "com.smartfren.switchmobile:id/txt_get_various_rewards";
+        String btnGoogle = "com.smartfren.switchmobile:id/btn_login_google";
+        String btnFacebook = "com.smartfren.switchmobile:id/btn_login_facebook";
+        String txtUserValidationTitle = "com.smartfren.switchmobile:id/title";
+        String btnChangeEmail = "com.smartfren.switchmobile:id/btn_change_email";
+        String btnSignUp2 = "com.smartfren.switchmobile:id/btn_signUp";
+        String txtLoginTitle = "com.smartfren.switchmobile:id/signup";
+        String txtErrorInputDesc = "com.smartfren.switchmobile:id/validation_txt";
+    }
+
+    public interface SignUpPageObject {
+        String btnLogin = "com.smartfren.switchmobile:id/login";
+        String btnLoginHere = "com.smartfren.switchmobile:id/login_here";
     }
 
     public interface PINPageObject {
-        WebElement txtPINPageTitle = driver.findElement(By.id("com.smartfren.switchmobile:id/textView"));
-        WebElement txtPINInputDesc = driver.findElement(By.id("com.smartfren.switchmobile:id/textView2"));
+        String txtPINPageTitle = "com.smartfren.switchmobile:id/textView";
+        String txtPINInputDesc = "com.smartfren.switchmobile:id/textView2";
         String inputPIN = "com.smartfren.switchmobile:id/pinnumber";
-        WebElement btnForgotPIN = driver.findElement(By.id("com.smartfren.switchmobile:id/textView3"));
-        WebElement txtOtherWaysTitle = driver.findElement(By.id("com.smartfren.switchmobile:id/textView5"));
-        WebElement txtLoginAnotherAccount = driver.findElement(By.id("com.smartfren.switchmobile:id/textView18"));
-        WebElement btnLoginAnotherAccount = driver.findElement(By.id("com.smartfren.switchmobile:id/clickhere"));
+        String btnForgotPIN = "com.smartfren.switchmobile:id/textView3";
+        String txtOtherWaysTitle = "com.smartfren.switchmobile:id/textView5";
+        String txtLoginAnotherAccount = "com.smartfren.switchmobile:id/textView18";
+        String btnLoginAnotherAccount = "com.smartfren.switchmobile:id/clickhere";
     }
 
     public interface MenuPageObject {
@@ -51,7 +61,11 @@ public class ObjectElement extends Driver {
     }
 
     public interface ProfilePageObject {
-        String txtDealsTilte = "com.smartfren.switchmobile:id/deals";
+        String txtHeaderTitle = "com.smartfren.switchmobile:id/profile";
+        String txtNameUserTitle = "com.smartfren.switchmobile:id/name_user";
+        String txtEmailUserTitle = "com.smartfren.switchmobile:id/user_email";
+        String btnEdit = "com.smartfren.switchmobile:id/edit_profile";
+        String txtDealsTitle = "com.smartfren.switchmobile:id/deals";
         String btnLogout = "com.smartfren.switchmobile:id/logout";
         String btnConfirmLogout = "android:id/button1";
     }
