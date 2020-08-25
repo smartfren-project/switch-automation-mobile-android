621ad6216e0ec30107ec31708bb6cfaefe4a921c
faizalmysf <67628652+faizalmysf@users.noreply.github.com>
2020-08-25 11:56:34 +0700

    configure environment

--- src/main/java/operation/BaseLogout.java
+++ src/main/java/operation/BaseLogout.java
@@ -1,15 +1,7 @@
 package operation;
 
-import constants.BaseData;
 import constants.ObjectElement;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.PerformsTouchActions;
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-
-import java.net.MalformedURLException;
 
 public class BaseLogout extends BaseTest {
 
